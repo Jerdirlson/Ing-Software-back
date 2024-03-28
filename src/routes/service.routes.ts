a6ad10ce079a005f95bf6212ca740699693ac793
Chris <128336431+ChisCurubo@users.noreply.github.com>
2024-03-27 23:38:20 -0500

    rutes service schedule and profile working continue

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { addServicesControler } from "../controllers/service.controller";
+import { addServicesControler, addServicesControlerDescip, deltServiceByName, getServiceAll, getServiceByName, getServiceId, updServicesById, updServicesDesc } from "../controllers/service.controller";
 const router : Router = Router();
 
 router
     .post('/addMedicControler', addServicesControler)
     .post('/addServicesControlerDescip',addServicesControlerDescip )
-    .p
+    .put('/updServicesById', updServicesById)
+    .put('/updServicesDesc', updServicesDesc)
+    .delete('/deltServiceByName', deltServiceByName)
+    .get('/getServiceId', getServiceId)
+    .get('/getServiceByName', getServiceByName)
+    .get('/getServiceAll',getServiceAll)
+
     
     
 export default router;
